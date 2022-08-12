d3417eb2796dd00fa49b3f47bdf794940d2cfcca
solidity <soliditygo@outlook.com>
2022-08-12 11:46:40 +0800

    init

---
+++ ethers-sign.js
@@ -0,0 +1,75 @@
+const ethers = require('ethers');
+const url = "http://127.0.0.1:8545";
+
+const main = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(url);
+    // ethers (create random new account)
+    let wallet = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider);
+
+    const msgParams = {
+        domain: {
+            chainId: '1',
+            name: 'Ether Mail',
+            verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+            version: '1',
+        },
+        message: {
+            contents: 'Hello, Bob!',
+            from: {
+                name: 'Cow',
+                wallets: [
+                    '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+                    '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+                ],
+            },
+            to: [
+                {
+                    name: 'Bob',
+                    wallets: [
+                        '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+                        '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+                        '0xB0B0b0b0b0b0B000000000000000000000000000',
+                    ],
+                },
+            ],
+        },
+        primaryType: 'Mail',
+        types: {
+            EIP712Domain: [
+                { name: 'name', type: 'string' },
+                { name: 'version', type: 'string' },
+                { name: 'chainId', type: 'uint256' },
+                { name: 'verifyingContract', type: 'address' },
+            ],
+            Group: [
+                { name: 'name', type: 'string' },
+                { name: 'members', type: 'Person[]' },
+            ],
+            Mail: [
+                { name: 'from', type: 'Person' },
+                { name: 'to', type: 'Person[]' },
+                { name: 'contents', type: 'string' },
+            ],
+            Person: [
+                { name: 'name', type: 'string' },
+                { name: 'wallets', type: 'address[]' },
+            ],
+        },
+    };
+
+    const sigTyped = await provider.send(
+        'eth_signTypedData_v4',
+        [wallet.address, JSON.stringify(msgParams)],
+    );
+
+    console.log(sigTyped)
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
